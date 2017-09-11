93737c697dd368e9602de5085cb38913811d8d06
Janos Dobi <j.dobi88@gmail.com>
2017-09-11 15:32:29 +0200

    hellouser, miletokmconverter, animalsandlegs

--- week2/Day-1/src/AnimalsAndLegs.java
+++ week2/Day-1/src/AnimalsAndLegs.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class AnimalsAndLegs {
   public static void main(String[] args) {
     // Write a program that asks for two integers
     // The first represents the number of chickens the farmer has
     // The seconf represents the number of pigs the farmer has
     // It should print how many legs all the animals have
+
+    System.out.println("Please enter the number of chickens: ");
+
+    Scanner input = new Scanner(System.in);
+    int numChicken = input.nextInt();
+
+    System.out.println("Please enter the number of pigs: ");
+    int numPig = input.nextInt();
+
+    System.out.println("The total number of animal legs on the farm is " + ((numChicken * 2) + (numPig * 4)));
+
   }
 }
--- week2/Day-1/src/MileToKmConverter.java
+++ week2/Day-1/src/MileToKmConverter.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class MileToKmConverter {
   public static void main(String[] args) {
     // Write a program that asks for an integer that is a distance in kilometers,
     // then it converts that value to miles and prints it
+
+    System.out.println("Distance in km/s: ");
+    Scanner input = new Scanner(System.in);
+    double distanceKm = input.nextInt();
+    System.out.println("The value you have entered equals to " + (distanceKm * 0.621371) + " miles");
+
   }
 }
