d168edf115d7fbd7dba08e2a4efb86e5a2232468
Janos Dobi <j.dobi88@gmail.com>
2017-09-11 14:24:18 +0200

    swap, favoritenumber

--- week2/Day-1/src/FavoriteNumber.java
+++ week2/Day-1/src/FavoriteNumber.java
@@ -3,5 +3,9 @@ public class FavoriteNumber {
     int favorite_number = 8;
     // Store your favorite number in a variable (as a number)
     // And print it like this: "My favorite number is: 8"
+
+    int myFavNum = 5768;
+    System.out.println("My favourite number is " + myFavNum);
+
   }
 }
