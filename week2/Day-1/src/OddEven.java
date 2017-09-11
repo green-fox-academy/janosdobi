5f746e9a2010074cd5111daac422ce09e7b91110
Janos Dobi <j.dobi88@gmail.com>
2017-09-11 16:01:22 +0200

    oddeven

--- week2/Day-1/src/OddEven.java
+++ week2/Day-1/src/OddEven.java
@@ -1,2 +1,21 @@
 // Write a program that reads a number form the standard input,
 // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+import java.util.Scanner;
+
+public class OddEven{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a whole number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
+
+
