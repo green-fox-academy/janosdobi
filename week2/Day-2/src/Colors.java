f789ac9bd1964f78ed6249aac2d2b1e7b747cc46
Janos Dobi <j.dobi88@gmail.com>
2017-09-12 15:37:18 +0200

    add sout to colors

--- week2/Day-2/src/Colors.java
+++ week2/Day-2/src/Colors.java
@@ -12,5 +12,10 @@ public class Colors {
 
         String[][] colors = {{"lime", "forest green", "olive", "pale green", "spring green"}, {"orange red", "red", "tomato"}, {"orchid", "violet", "pink", "hot pink"}};
 
+        for(int i = 0; i < colors.length; i++) {
+            for(int j = 0; j < colors[i].length; j++) {
+                System.out.println(colors[i][j]);
+            }
+        }
     }
 }
