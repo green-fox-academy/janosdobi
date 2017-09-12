9cfc770a8cc6b683471295d11783870e78b26451
Janos Dobi <j.dobi88@gmail.com>
2017-09-12 14:45:08 +0200

    doubleitems, appenda2

---
+++ week2/Day-2/src/AppendA2.java
@@ -0,0 +1,19 @@
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
+
+public class AppendA2 {
+    public static void main(String[] args) {
+
+        String[] nimals = {"kuty", "macsk", "cic"};
+
+        for(int i = 0; i < nimals.length; i++) {
+            nimals[i] += "a";
+        }
+
+        for(int i = 0; i < nimals.length; i++) {
+            System.out.println(nimals[i]);
+        }
+
+    }
+}
