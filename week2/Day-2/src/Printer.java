1f68d6cbf97a52cf34824a27e821eed9e3edadef
Janos Dobi <j.dobi88@gmail.com>
2017-09-12 13:54:28 +0200

    printer, third

---
+++ week2/Day-2/src/Printer.java
@@ -0,0 +1,26 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `printer`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printer("first")
+// printer("first", "second")
+// printer("first", "second", "third", "fourh")
+// ...
+
+public class Printer {
+    public static void main(String[] args) {
+
+        printer("first", "second", "third");
+
+    }
+
+    public static void printer(String... strings) {
+        for (String i : strings) {
+            System.out.println(i);
+        }
+   }
+}
---
+++ week2/Day-2/src/Third.java
@@ -0,0 +1,12 @@
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+public class Third {
+    public static void main(String[] args) {
+
+        int[] myArray = {4, 5, 6, 7};
+        System.out.println(myArray[2]);
+
+    }
+}
