ec332a236419e2d10c16972a3e19dd3b7443a231
Janos Dobi <j.dobi88@gmail.com>
2017-09-13 12:45:33 +0200

    matchmaking

---
+++ week2/Day-3/src/Matchmaking.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Matchmaking{
+    public static void main(String... args){
+        ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
+        ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+        ArrayList<String> order = new ArrayList<String>();
+
+        // Join the two lists by matching one girl with one boy in the order list
+        // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+
+        for(int i = 0; i < 5; i++) {
+            order.add(girls.get(i));
+            order.add(boys.get(i));
+        }
+        order.add(boys.get(5));
+        
+        System.out.println(order);
+
+        }
+
+
+    }
