a9af03acbbe2541ba0dc4031e8519d5aaf6499f6
Janos Dobi <j.dobi88@gmail.com>
2017-09-14 10:51:52 +0200

    Drawpyramid

--- week2/Day-1/src/DrawPyramid.java
+++ week2/Day-1/src/DrawPyramid.java
@@ -9,15 +9,25 @@
 //
 // The pyramid should have as many lines as the number was
 
-
-import javax.sound.midi.Soundbank;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Dra{
+public class DrawPyramid {
     public static void main(String[] args) {
-        Arrays
-    }
-    }
+
+        System.out.println("Please enter a number: ");
+        Scanner input = new Scanner(System.in);
+        int line = input.nextInt();
+
+        for (int i = 1 ; i <= line ; i++) {
+            for (int j = 1 ; j < line ; j++ )
+                System.out.print(" ");
+            line--;
+
+            for (int j = 1 ; j <= 2*i - 1 ; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
+
