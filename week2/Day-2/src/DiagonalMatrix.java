7b6f9f6a46845543b780a32e9acfd80f1e69b127
Janos Dobi <j.dobi88@gmail.com>
2017-09-14 17:08:15 +0200

    correct diagonalmatrix as to comments

--- week2/Day-2/src/DiagonalMatrix.java
+++ week2/Day-2/src/DiagonalMatrix.java
@@ -11,21 +11,21 @@
 public class DiagonalMatrix {
     public static void main(String[] args) {
 
-        int[][] Matrix = new int[4][4];
+        int[][] matrix = new int[4][4];
 
         for(int i = 0; i < 4; i++) {
             for(int j = 0; j < 4; j++)
                 if(i == j) {
-                    Matrix[i][j] = 1;
+                    matrix[i][j] = 1;
                 } else {
-                    Matrix[i][j] = 0;
+                    matrix[i][j] = 0;
                 }
-        }
+            }
 
         for(int i = 0; i < 4; i++) {
             System.out.println("");
             for (int j = 0; j < 4; j++) {
-                System.out.print(Matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
         }
     }
