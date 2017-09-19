2b5cb421c83b0a6b6f11121c4623ce72ac3a38e9
Janos Dobi <j.dobi88@gmail.com>
2017-09-19 19:47:26 +0200

    writing in file in doubled excercise

--- week3/Day-2/src/Doubled.java
+++ week3/Day-2/src/Doubled.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Doubled {
@@ -16,16 +17,17 @@ public class Doubled {
     public static void decrypt(String fileName){
 
         Path myPath = Paths.get(fileName);
-        String output= "";
+                ArrayList<String> outputList = new ArrayList<>();
 
         try {
             for (String lines: Files.readAllLines(myPath)) {
+                String output= "";
                 for (int i = 0; i < lines.length(); i+= 2) {
                     output += lines.charAt(i);
                 }
-                output += "\n";
+                outputList.add(output);
             }
-            System.out.println(output);
+            Files.write(myPath, outputList);
 
         } catch (IOException e) {
 
