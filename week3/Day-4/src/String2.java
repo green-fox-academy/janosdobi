db06aa967c57b47394a6258150a164c778592c64
Janos Dobi <j.dobi88@gmail.com>
2017-09-21 12:40:02 +0200

    string2 excercise

---
+++ week3/Day-4/src/String2.java
@@ -0,0 +1,14 @@
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+public class String2 {
+    public static void main(String[] args) {
+        System.out.println(string2("xaxaxaxax"));
+    }
+    public static String string2(String input) {
+        if(input.length() == 0) {
+            return "";
+        }
+        String firstChar = input.startsWith("x") ? "" : input.substring(0, 1);
+        return firstChar +  string2(input.substring(1));
+    }
+}
