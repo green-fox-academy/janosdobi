64f91832620092952701b89bee7eb7ce4aa53950
Janos Dobi <j.dobi88@gmail.com>
2017-09-25 16:17:48 +0200

    sharpies upgraded with SharpieSet class

--- Week4/Day-1/src/Sharpie.java
+++ Week4/Day-1/src/Sharpie.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sharpie {
     String color;
     float width;
@@ -13,11 +16,50 @@ public class Sharpie {
     }
 }
 
+class SharpieSet {
+
+    List<Sharpie> sharpies = new ArrayList<>();
+
+    public SharpieSet (){
+    }
+
+    int countUsable() {
+        int count = 0;
+        for (int i = 0; i < sharpies.size(); i++) {
+            if (sharpies.get(i).inkAmount > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void removeTrash() {
+        for (int i = 0; i < sharpies.size(); i++) {
+            if (sharpies.get(i).inkAmount == 0) {
+                sharpies.remove(i);
+            }
+        }
+    }
+
+    void add (Sharpie sharpie) {
+        sharpies.add(sharpie);
+    }
+
+
+}
+
 class Main3 {
     public static void main(String[] args) {
         Sharpie redBig = new Sharpie("red", (float) 43.6);
         redBig.use();
         System.out.println(redBig.inkAmount);
         System.out.println(redBig.color);
+
+        SharpieSet sharpiesList = new SharpieSet();
+        sharpiesList.add(redBig);
+
+        System.out.println(sharpiesList.countUsable());
+
+
     }
 }
