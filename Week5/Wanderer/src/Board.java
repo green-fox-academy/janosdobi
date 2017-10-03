e7a7562158a54646b104cc668a4d73f5ce8e0e54
Janos Dobi <j.dobi88@gmail.com>
2017-10-03 14:19:20 +0200

    add hero+movements

--- Week5/Wanderer/src/Board.java
+++ Week5/Wanderer/src/Board.java
@@ -9,16 +9,16 @@ public class Board extends JComponent implements KeyListener {
     int posY;
     int wallPosX;
     int wallPosY;
+    int heroPosY;
+    int heroPosX;
 
 
     public Board() {
-        posX = 0;
-        posY = 0;
-        wallPosX = 72 * 3;
-        wallPosY = 0;
+        heroPosY = 0;
+        heroPosX = 0;
 
         // set the size of your draw board
-        setPreferredSize(new Dimension(720, 720));
+        setPreferredSize(new Dimension(720, 792));
         setVisible(true);
     }
 
@@ -26,7 +26,9 @@ public class Board extends JComponent implements KeyListener {
     public void paint(Graphics graphics) {
         super.paint(graphics);
         // Drawing floor
-        for (int i = 0; i < 10; i++) {
+        posX = 0;
+        posY = 0;
+        for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 10; j++) {
                 PositionedImage image = new PositionedImage("images/floor.png", posX, posY);
                 image.draw(graphics);
@@ -36,6 +38,8 @@ public class Board extends JComponent implements KeyListener {
             posY += 72;
         }
         //Drawing walls
+        wallPosX = 3 * 72;
+        wallPosY = 0;
         for (int i = 0; i < 2; i++) {
             PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
             image.draw(graphics);
@@ -53,11 +57,17 @@ public class Board extends JComponent implements KeyListener {
             image.draw(graphics);
             wallPosY += 72;
         }
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 3; i++) {
             PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
             image.draw(graphics);
             wallPosX += 72;
         }
+        for (int i = 0; i < 6; i++) {
+            PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
+            image.draw(graphics);
+            wallPosY += 72;
+        }
+
         wallPosY = 4 * 72;
         wallPosX = 0;
         for (int i = 0; i < 3; i++) {
@@ -78,7 +88,53 @@ public class Board extends JComponent implements KeyListener {
             wallPosY += 72;
         }
 
+        wallPosY = 8 * 72;
+        wallPosX = 72;
+        for (int i = 0; i < 2; i++) {
+            PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
+            image.draw(graphics);
+            wallPosX += 72;
+        }
+        for (int i = 0; i < 3; i++) {
+            PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
+            image.draw(graphics);
+            wallPosY += 72;
+        }
+
+        wallPosY = 10 * 72;
+        wallPosX = 5 * 72;
+        for (int i = 0; i < 1; i++) {
+            PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
+            image.draw(graphics);
+            wallPosY -= 72;
+        }
+        for (int i = 0; i < 2; i++) {
+            PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
+            image.draw(graphics);
+            wallPosX += 72;
+        }
+        drawSquareWall(graphics, 7 * 72, 72, 2);
+        drawSquareWall(graphics, 5 * 72, 6 * 72, 2);
+        drawSquareWall(graphics, 72, 10 * 72, 1);
+
+        //draw hero
+        PositionedImage hero = new PositionedImage("images/hero-down.png", heroPosX, heroPosY);
+        hero.draw(graphics);
+
     }
+
+        public void drawSquareWall(Graphics g, int wallPosX, int wallPosY, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                PositionedImage image = new PositionedImage("images/wall.png", wallPosX, wallPosY);
+                image.draw(g);
+                wallPosX += 72;
+            }
+            wallPosX -= 2 * 72;
+            wallPosY += 72;
+        }
+    }
+
 
     // To be a KeyListener the class needs to have these 3 methods in it
     @Override
@@ -96,13 +152,13 @@ public class Board extends JComponent implements KeyListener {
     public void keyReleased(KeyEvent e) {
         // When the up or down keys hit, we change the position of our box
         if (e.getKeyCode() == KeyEvent.VK_W) {
-            posY -= 100;
+            heroPosY -= 72;
         } else if(e.getKeyCode() == KeyEvent.VK_S) {
-            posY += 100;
+            heroPosY += 72;
         } else if(e.getKeyCode() == KeyEvent.VK_D) {
-            posX += 100;
+            heroPosX += 72;
         } else if(e.getKeyCode() == KeyEvent.VK_A) {
-            posX -= 100;
+            heroPosX -= 72;
         }
         // and redraw to have a new picture with the new coordinates
         repaint();
