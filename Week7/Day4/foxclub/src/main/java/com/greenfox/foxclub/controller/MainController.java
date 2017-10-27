3aec3098e05e34a6ffc6b69dd29ed2670e070fb7
Janos Dobi <j.dobi88@gmail.com>
2017-10-27 10:06:59 +0200

    fox club project init

---
+++ Week7/Day4/foxclub/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.greenfox.foxclub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value={"", "/"})
+    public String index(Model model) {
+        model.addAttribute("fox");
+        return "index";
+    }
+}
