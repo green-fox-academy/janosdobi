a9dcfe8265b5e12f388e6aea900bfb2330bcb0d1
Janos Dobi <j.dobi88@gmail.com>
2017-11-03 15:12:11 +0100

    service class annotation

--- Week8/Day5/reddit/src/main/java/com/greenfox/reddit/service/PostService.java
+++ Week8/Day5/reddit/src/main/java/com/greenfox/reddit/service/PostService.java
@@ -3,8 +3,9 @@ package com.greenfox.reddit.service;
 import com.greenfox.reddit.model.Post;
 import com.greenfox.reddit.repositories.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class PostService {
 
     @Autowired
--- Week8/Day5/reddit/src/main/java/com/greenfox/reddit/service/UserService.java
+++ Week8/Day5/reddit/src/main/java/com/greenfox/reddit/service/UserService.java
@@ -3,7 +3,10 @@ package com.greenfox.reddit.service;
 import com.greenfox.reddit.model.User;
 import com.greenfox.reddit.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserService {
 
     @Autowired
