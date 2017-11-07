413880f8a85cc53e148280dea6daff55d1cb4014
Janos Dobi <j.dobi88@gmail.com>
2017-11-07 11:44:41 +0100

    greeter exercise done, doubling enhanced w try-catch forr error message

--- Week9/Day2/restproject/src/main/java/com/greenfox/restproject/controllers/RestController.java
+++ Week9/Day2/restproject/src/main/java/com/greenfox/restproject/controllers/RestController.java
@@ -9,13 +9,29 @@ public class RestController {
 
     @GetMapping("/doubling")
     @ResponseBody
-    public String doubling(@RequestParam(value="input", defaultValue = "0") Long received) {
+    public String doubling(@RequestParam(value="input", required = false) Long received) {
         String error = "Please provide an input!";
-        if(!(received == 0)) {
+        try {
             Long result = received * 2;
             return String.valueOf(result);
-        } else {
+        } catch (NullPointerException e){
             return error;
         }
     }
+
+    @GetMapping("/greeter")
+    @ResponseBody
+    public String greeter(@RequestParam(value="name", required = false) String name,
+                          @RequestParam(value="title", required = false) String title) {
+        String welcome_message = String.format("Oh, hi there %s, my dear %s!", name, title);
+        String error = "Please provide a name!";
+        try {
+            return welcome_message;
+        } catch(NullPointerException e) {
+            return error;
+        }
+    }
+
+    
+
 }
